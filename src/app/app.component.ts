76136008631d0a8f27739c52f7b64119a5edf3fd
KarlSchmitt <Karl.Schmitt@deutschebahn.com>
2023-10-02 08:43:02 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'playground_Oct_2nd_2023';
+}
